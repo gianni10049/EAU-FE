1d6a91cc495e71c024909daeb4178cbbd9267add
baki22 <angelopane22@gmail.com>
2022-12-15 20:18:42 +0000

    Added Donate page

---
+++ src/components/Donate/donate.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+
+
+const Donate = () => {
+	const { t } = useTranslation();
+
+
+	return (
+		<>Pippo anche io</>
+
+
+		);
+};
+
+export default Donate;
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,12 +1,21 @@
 import Homepage from '../components/Homepage/homepage';
 import React from 'react';
 import {routeData} from './routes.model';
+import Donate from '../components/Donate/donate';
 
 const routes: routeData[] = [
 	{
 		path: '',
 		key: 'homepage',
 		component: <Homepage />,
+		account_needed: false,
+		nav: true,
+	},
+
+	{
+		path: '/donate',
+		key: 'donate',
+		component: <Donate />,
 		account_needed: false,
 		nav: true,
 	}
